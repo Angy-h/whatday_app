b6e7582f47bebf862a71f1a1dff493ca11889186
Annaheim.h@gmail.com <Anna Hiragi>
2024-06-09 01:14:00 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'sessions/new'
+  # get 'users/new'
   root "static_pages#home"
   get  "/news",    to: "static_pages#news"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get  "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
 end
