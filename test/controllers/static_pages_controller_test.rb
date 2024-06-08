57690fc965fc4f118163ed2ed6a015a12d0b1250
Annaheim.h@gmail.com <Anna Hiragi>
2024-06-08 22:13:46 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,20 +2,26 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Whatday Today App"
   end
 
   test "should get news" do
-    get static_pages_news_url
+    get news_path
     assert_response :success
     assert_select "title", "News | Whatday Today App"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Whatday Today App"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Whatday Today App"
   end
 end
