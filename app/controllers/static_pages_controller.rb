c71ccc05d9bad3de1511a5b6f67572ae8ee1bec7
Annaheim.h@gmail.com <Anna Hiragi>
2024-06-30 17:57:55 +0900

    add display day-information

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
+
+    @days_info = DaysInfo.find_by(date: Date.today.strftime("%Y-%m-%d"))
   end
 
   def news
